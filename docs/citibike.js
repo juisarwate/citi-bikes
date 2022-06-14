c248e722574f116a7c25f6f6717bda2d218a551e
js5966 <93935138+js5966@users.noreply.github.com>
2022-06-14 09:22:40 -0400

    Update js and html

--- docs/citibike.js
+++ docs/citibike.js
@@ -1,14 +1,24 @@
 mapboxgl.accessToken = "pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ";
-var gradmap = new mapboxgl.Map({
+var citimap = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/jsarwate/cl3uhu68k001f14n9t35kdhva",
-  zoom: 3,
-  maxZoom: 9,
-  minZoom: 3.5,
-  center: [-99, 38],
-  maxBounds: [
-    [-180, 15],
-    [-30, 72],
-  ],
-  projection: "albers",
+  zoom: 10,
+  center: [-74, 40.725]
+});
+
+citimap.on('load', function () {
+    citimap.addLayer({
+        'id': 'citibikeData',
+        'type': 'circle',
+        'source': {
+            'type': 'geojson',
+            'data': 'data/startgeo_2020.geojson'
+        },
+        'paint': {
+            'circle-color': '#ff7f50',
+            'circle-stroke-color': '#4d4d4d',
+            'circle-stroke-width': 0.5,
+            'circle-radius': 5
+        }
+    });
 });
